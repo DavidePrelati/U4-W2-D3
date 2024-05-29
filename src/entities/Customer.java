ef79942efc51e3ad32336721f17ab442a01798f0
DavidePrelati <davide.prelati@gmail.com>
2024-05-29 16:10:27 +0200

    creazione classi

---
+++ src/entities/Customer.java
@@ -0,0 +1,36 @@
+package entities;
+
+import java.util.Random;
+
+public class Customer {
+
+    private String name;
+    private Long id;
+    private Integer tier;
+
+    public Customer(String name, Integer tier) {
+        this.name = name;
+        this.id = randomId();
+        this.tier = tier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
+
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
+
+    private Long randomId() {
+        return new Random().nextLong();
+    }
+}
